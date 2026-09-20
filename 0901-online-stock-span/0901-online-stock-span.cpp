291db2e75d867a0223cc65a0bcadbcc8a5ed2661
D Karthik Reddy <karthikdreddy501@gmail.com>
2026-09-20 10:34:22 +0530

    Time: 40 ms (29.9%), Space: 97.2 MB (23.09%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.cpp
+++ 0901-online-stock-span/0901-online-stock-span.cpp
@@ -1,25 +1,19 @@
 class StockSpanner {
 public:
-    vector<int> prices;
+    stack<pair<int,int>> st;
 
     StockSpanner() {
         
     }
     
     int next(int price) {
-        prices.push_back(price);
-
-        int span = 0;
-
-        for (int i = prices.size() - 1; i >= 0; i--) {
-            if (prices[i] <= price) {
-                span++;
-            }
-            else {
-                break;
-            }
+        int span=1;
+        while(!st.empty() && st.top().first<=price){
+            span+=st.top().second ;
+            st.pop(); 
         }
-
+        st.push({price,span});
         return span;
+        
     }
 };
